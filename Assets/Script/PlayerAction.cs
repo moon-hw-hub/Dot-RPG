e097b6f31e72dbcca4665403ea8447486e6c1e43
Mhw <princehw03@gmail.com>
2025-08-28 18:21:52 +0900

    오브젝트 상호작용 스크립트까지 구현

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -6,6 +6,8 @@ public class PlayerAction : MonoBehaviour
     float h;
     float v;
     bool isHorizonMove;
+    Vector3 dirVec; // 현재 바라보고 있는 방향 값을 가진 변수가 필요
+    GameObject scanObject; // 상호작용 중인 오브젝트
 
     Rigidbody2D rigid;
     Animator anim;
@@ -49,6 +51,21 @@ public class PlayerAction : MonoBehaviour
         }
         else
             anim.SetBool("isChange", false);
+
+        //Direction
+        if (vDown && v==1)
+            dirVec = Vector3.up;
+        else if (vDown && v == -1)
+            dirVec = Vector3.down;
+        else if (hDown && h == -1)
+            dirVec = Vector3.left;
+        else if (hDown && h == 1)
+            dirVec = Vector3.right;
+
+        // Scan Object
+        if(Input.GetButtonDown("Jump") && scanObject != null) 
+            Debug.Log("this is : " + scanObject.name);
+
     }
 
     void FixedUpdate()
@@ -56,6 +73,16 @@ public class PlayerAction : MonoBehaviour
         //Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v); // 대각선 이동 방지
         rigid.linearVelocity = moveVec * moveSpeed;
+
+        //Ray
+        Debug.DrawRay(rigid.position, dirVec*0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if(rayHit.collider != null)
+            scanObject = rayHit.collider.gameObject;
+        else
+            scanObject = null;
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
