2228f1dd881ea216ac64d0d4a0c6b4d3d2ab6f96
Mhw <princehw03@gmail.com>
2025-08-31 15:46:37 +0900

    대화 시스템 구현

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject talkPanel;
+    public Image portraitImg;
     public TextMeshProUGUI talkText;
     public GameObject scanObject;
     public bool isAction; // 상호작용 중인지 아닌지 판단
+    public int talkIndex; // 대화 인덱스
 
     public static GameManager instance = null;
     void Awake()
@@ -21,18 +23,40 @@ public class GameManager : MonoBehaviour
 
     public void Action(GameObject scanObj)
     {
-        if (isAction) // Enter Action
+        isAction = true;
+        scanObject = scanObj;
+        ObjData objData = scanObject.GetComponent<ObjData>(); // 오브젝트의 데이터를 가져옴
+        Talk(objData.id, objData.isNpc); // 대화
+        //Debug.Log(objData.id + ", " + objData.isNpc);
+
+        talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        string talkData = TalkManager.instance.GetTalk(id, talkIndex);
+
+        if (talkData == null) // 더이상 남아있는 문장이 없음
         {
             isAction = false;
+            talkIndex = 0;
+            return; // 대화 끝
+        }
+        if (isNpc)
+        {
+            talkText.text = talkData.Split(':')[0];
+
+            portraitImg.sprite = TalkManager.instance.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImg.color = new Color(1, 1, 1, 1); // 알파값 1로 변경
         }
         else
         {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것의 이름은 " + scanObject.name + "이라고 한다.";
+            talkText.text = talkData;
+            portraitImg.color = new Color(1, 1, 1, 0);
         }
 
-        talkPanel.SetActive(isAction);
+        isAction = true;
+        talkIndex++;
     }
 
 }
